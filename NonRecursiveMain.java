a026ce3b21e9aca5335c3d388e2fe1c6e0126f97
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 19:45:55 +0530

    Main Class is created

--- NonRecursiveMain.java
+++ NonRecursiveMain.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class MyMain{
+class NonRecursiveMain{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int size = sc.nextInt();
